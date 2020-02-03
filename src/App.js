e31653cd157b06871e49866555fbe1a85d682f9a
Dashon Jamal Hawkins <dashon.hawkins@gmail.com>
2020-02-03 07:03:48 -0700

    Added 'src' + 'public' folders & associated files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is my Todoist clone app.</p>
+      </header>
+    </div>
+  );
+}
